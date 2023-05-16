94ce5c04fd98e5486beca13d4d92bc5fc69fbddb
Unnamed <unnamed@undefined.com>
2023-05-16 23:08:08 +0200

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import AuthBox from "./auth/components/AuthBox";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-    return <AuthBox />;
+    return (
+        <Grid
+            templateAreas={{
+                base: `"topnav" "main"`,
+                lg: `"topnav topnav" "sidenav main"`,
+            }}
+            templateColumns={{
+                base: "1fr",
+                lg: "200px 1fr",
+            }}
+        >
+            <GridItem area="topnav"></GridItem>
+            <Show above="lg">
+                <GridItem area="sidenav"></GridItem>
+            </Show>
+            <GridItem area="main"></GridItem>
+        </Grid>
+    );
 }
 
 export default App;
