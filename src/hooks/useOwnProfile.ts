306e7853ab1a180b52505fce9f9651520ac30fee
Unnamed <unnamed@undefined.com>
2023-07-19 18:24:23 +0200

    Make useOwnProfile hook return null if user is not authenticated

--- src/hooks/useOwnProfile.ts
+++ src/hooks/useOwnProfile.ts
@@ -1,13 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
+import { AxiosError, HttpStatusCode } from "axios";
 import Profile from "../entities/Profile";
 import ApiClient from "../services/ApiClient";
 
 const apiClient = new ApiClient<Profile>("/profiles/profiles/me/");
 
 function useOwnProfile() {
-    return useQuery<Profile, Error>({
+    return useQuery<Profile | null, AxiosError>({
         queryKey: ["own-profile"],
-        queryFn: () => apiClient.get(),
+        queryFn: () =>
+            apiClient.get().catch((error) => {
+                if (error instanceof AxiosError && error.response) {
+                    const status = error.response.status as HttpStatusCode;
+                    if (status === HttpStatusCode.Unauthorized) return null;
+                }
+                throw error;
+            }),
     });
 }
 
