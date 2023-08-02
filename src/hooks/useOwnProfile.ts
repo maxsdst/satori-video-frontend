86500c424dddd98fc9d6156b9b1b7228c084a50d
Unnamed <unnamed@undefined.com>
2023-08-02 23:57:47 +0200

    Clear auth tokens when fetching the own profile fails with a 401 status code

--- src/hooks/useOwnProfile.ts
+++ src/hooks/useOwnProfile.ts
@@ -1,18 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import { AxiosError, HttpStatusCode } from "axios";
+import useLogout from "../auth/hooks/useLogout";
 import Profile from "../entities/Profile";
 import ApiClient from "../services/ApiClient";
 
 const apiClient = new ApiClient<Profile>("/profiles/profiles/me/");
 
 function useOwnProfile() {
+    const logout = useLogout();
+
     return useQuery<Profile | null, AxiosError>({
         queryKey: ["own-profile"],
         queryFn: () =>
             apiClient.get().catch((error) => {
                 if (error instanceof AxiosError && error.response) {
                     const status = error.response.status as HttpStatusCode;
-                    if (status === HttpStatusCode.Unauthorized) return null;
+                    if (status === HttpStatusCode.Unauthorized) {
+                        logout();
+                        return null;
+                    }
                 }
                 throw error;
             }),
