09e5ebd5d126a052bbbfcecf5651697c41ee136d
Unnamed <unnamed@undefined.com>
2024-10-15 07:01:23 +0200

    Customize theme colors

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
     initialColorMode: "dark",
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+    config,
+    colors: {
+        gray: {
+            50: "#eff1f5",
+            100: "#d2d5dc",
+            200: "#b4b9c6",
+            300: "#959eb1",
+            400: "#77829c",
+            500: "#5e6884",
+            600: "#495166",
+            700: "#353a48",
+            800: "#20232b",
+            900: "#0a0c10",
+        },
+    },
+});
 
 export default theme;
